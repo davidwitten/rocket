ca01cf50103860d1a0d7220c1dfca5800b5e38f6
Noah Kim <noahbkim@gmail.com>
2014-11-18 18:16:03 -0500

    Started on the Lexer

--- README.md
+++ README.md
@@ -1,38 +1,4 @@
-rocket
+Rocket
 ======
 
-A state-of-the-art, revolutionary algorithmic language, it incorporates next-level syntax, inspiring innovation.
-It is changing the world one program at a time.
-
-Syntax
---------
-      ! : output
-      ? : input
-      @ : load (with variable) (_variable to denote list)
-
-      + : addition
-      - : subtraction
-      * : multiplication
-      / : division
-      % : modulus
-      ^ : exponentiation
-
-      $ : while-loop
-      ~ : if-statement
-      
-      LIST:
-      & : add element
-      | : sorted list
-
-Opinions
---------
-"This language is nasty"- Everyone
-
-"I should use this instead of Python"- Arman S.
-
-Credits
--------
-
-Created by: 
-* Noah K @noahbkim
-* David W @dwizard
+A revolutionary new programming language oriented towards learning algorithms.
--- rocket.py
+++ rocket.py
@@ -1,157 +1,70 @@
-#Rocket
-#David W, Noah K
+# Rocket
+# David Witten, Noah Kim
 
-#Import
-import string
+# Import
+import fractions
+from ply import lex
+from ply import yacc
 
-"""
-Syntax
-! : output
-? : input
-@ : load (with variable) (_variable to denote list)
+# Lex
+tokens = (
+    "NAME",
 
-+ : addition
-- : subtraction
-* : multiplication
-/ : division
-% : modulus
-^ : exponentiation
+    "NUMBER", "STRING",
 
-$ : while-loop
-~ : if-statement
+    "EQUALS",
+    "PLUS", "MINUS", "TIMES", "DIVIDE", "MOD", "POWER",
+    "OR", "AND", "NOT",
+    "LT", "LE", "GT", "GE", "EQ", "NE",
 
-LIST:
-& : add element
-| : sorted list
-"""
+    "LPAREN", "RPAREN",
+    "LBRACKET", "RBRACKET",
+    "COMMA", "PERIOD",
+    "SEMI", "COLON",
+)
 
-Local,Lists = {v: 0 for v in string.ascii_lowercase},{'_' + v:[] for v in string.ascii_lowercase}
-Local.update(Lists)
-current = ''
-def Mapit(List,Operation):
-    Operation = Operation[0] + str(eval(Operation[1:],Local))
-    Operation = Operation.replace('^','**')
-    return list(map(lambda x: eval('x' + Operation),List))
-def interpret(line):
-    global current
-    global Local
-    themain = ('_' in current)
-    if not(themain):
-        line = line.replace('^','**')
-    if line.startswith("!"):
-        if line[1] != '_':
-            print(eval(line[1:], Local))
-        else:
-            print(' '.join([str(i) for i in eval(line[1:],Local)]))
-    elif line.startswith("?"):
-        Local[line[1]] = eval(input("?%s " % line[1]))
-    elif line.startswith("@"):
-        current = line[1:]
-    elif line.startswith("~"):
-        if eval(str(Local[current]) + line[1:line.index(':')]):
-            interpret(line[line.index(":") + 1:])
-    elif line.startswith("="):
-        assert current
-        if not(themain) and '[' in line:#non-list
-            raise TypeError("Integral variable name must have integral value")
-        Local[current] = eval(line[1:], Local)
-    elif line.startswith("&"):
-        assert current
-        if themain:
-            if type(eval(line[1:],Local))!=list:
-                Local[current].append(eval(line[1:],Local))
-            else:
-                Local[current].extend(eval(line[1:],Local))
-        else:
-            raise TypeError("List only function")
-    elif line.startswith('|'):
-        assert current
-        if not(themain):
-            raise TypeError("List only function")
-        Local[current] = sorted(Local[current])
-    elif line.startswith("#") or line == "":
-        pass
-    else:
-        assert current
-        if not(themain):
-            
-            Local[current] = eval(str(Local[current])+str(line),Local)
-        else:
-            Local[current] = Mapit(Local[current],line)
+t_NAME = r"[a-zA-Z_][0-9a-zA-Z_]*"
 
-##        raise SyntaxError("Bad syntax: %s" % line)
+def t_NUMBER(t):
+    r"\d+(\.\d+)?"
+    t.value = fractions.Fraction(t.value)
+    return t
 
-def main(script):
-    global current
-    global Local
-    trigger = False #True if in while loop
+t_STRING = r'"[^"\\\n]*(\\.[^"\\\n]*)*"'
+    
+t_EQUALS = r"="
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_TIMES = r"\*"
+t_DIVIDE = r"/"
+t_MOD = r"%"
+t_POWER = r"\^"
+t_OR = r"or"
+t_AND = r"and"
+t_NOT = r"not"
+t_LT = r"<"
+t_LE = r"<="
+t_GT = r">"
+t_GE = r">="
+t_EQ = r"=="
+t_EQ = r"<>"
 
-    lines = [line.strip() for line in script.split(';')]
-    for place,line in enumerate(lines):
-        if line.startswith("$"):
-            trigger = True
-            g = lines[place+1:lines.index("END")]
-            assert current
-            while eval(str(Local[current]) + line[1:line.index(':')]): #while CURRENT (comparison) :
-                for inloop in g:
-                    interpret(inloop)
-        elif line == 'END':
-            trigger = False
-        else:
-            if not(trigger):
-                interpret(line)
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_LBRACKET = r"\["
+t_RBRACKET = r"\]"
+t_COMMA = r","
+t_PERIOD = r"\."
+t_SEMI = r";"
+t_COLON = r":"
 
-###
-'''Edit script below'''
-### 
+def t_NEWLINE(self, t):
+        r"\n+"
+        t.lexer.lineno += t.value.count("\n")
 
-fibonacci = '''
-@a;
-=0;
-@b;
-=1;
-!b;
-@a;
+t_ignore = " \t"
 
-$<20000:;
+def t_error(t):
+    print("unknown character: %s" % t.value)
 
-@c;=b+a;
-@a;=b;
-@b;=c;
-!b;
-
-END;
-'''
-
-TestIf = """
-@a;
-=3;
-~<=4:*5;
-!a;
-"""
-TestMapping= '''
-@_a;
-=[1,2,3,4,5];
-^5;
-*15;
-!_a;'''
-
-TestAppend = '''
-@_a;
-=[1,2,3,4,5];
-&3;
-&[1,2];
-!_a;
-'''
-
-TestSort = '''
-@_a;
-=[1,2,3,2,1];
-|;
-!_a;
-'''
-
-script = '''
-'''
-
-main(script)
+lex.lex()
