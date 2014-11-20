1f114db5a34ba628b7b8312e118de9dd10fa608c
Noah Kim <noahbkim@gmail.com>
2014-11-19 21:48:30 -0500

    New Syntax
    No implementation

--- rocket.py
+++ rocket.py
@@ -1,70 +1,54 @@
 # Rocket
 # David Witten, Noah Kim
 
+"""
+
+Rocket Syntax
+
+Identities
+<a-z>   number variables
+<A-Z>   list variables
+
+Identity Operations
+expression->a     set number or list
+a                 get number
+expression->a:0   set list element
+a:0               get list element 
+
+Number Operations
++   add
+-   subtract
+*   multiply
+/   divide
+%   mod
+^   power
+
+List Operations
+$   sort
+
+
+Comparison
+=    equal 
+>    greater
+<    less
+
+Boolean
+&   and
+|   or
+~   not
+
+Grouping
+(...)   higher precedence operation
+
+Control
+?(<expression>){...}   if
+!(<expression>){...}   while
+
+
+"""
+
 # Import
 import fractions
+
 from ply import lex
 from ply import yacc
-
-# Lex
-tokens = (
-    "NAME",
-
-    "NUMBER", "STRING",
-
-    "EQUALS",
-    "PLUS", "MINUS", "TIMES", "DIVIDE", "MOD", "POWER",
-    "OR", "AND", "NOT",
-    "LT", "LE", "GT", "GE", "EQ", "NE",
-
-    "LPAREN", "RPAREN",
-    "LBRACKET", "RBRACKET",
-    "COMMA", "PERIOD",
-    "SEMI", "COLON",
-)
-
-t_NAME = r"[a-zA-Z_][0-9a-zA-Z_]*"
-
-def t_NUMBER(t):
-    r"\d+(\.\d+)?"
-    t.value = fractions.Fraction(t.value)
-    return t
-
-t_STRING = r'"[^"\\\n]*(\\.[^"\\\n]*)*"'
-    
-t_EQUALS = r"="
-t_PLUS = r"\+"
-t_MINUS = r"-"
-t_TIMES = r"\*"
-t_DIVIDE = r"/"
-t_MOD = r"%"
-t_POWER = r"\^"
-t_OR = r"or"
-t_AND = r"and"
-t_NOT = r"not"
-t_LT = r"<"
-t_LE = r"<="
-t_GT = r">"
-t_GE = r">="
-t_EQ = r"=="
-t_EQ = r"<>"
-
-t_LPAREN = r"\("
-t_RPAREN = r"\)"
-t_LBRACKET = r"\["
-t_RBRACKET = r"\]"
-t_COMMA = r","
-t_PERIOD = r"\."
-t_SEMI = r";"
-t_COLON = r":"
-
-def t_NEWLINE(self, t):
-        r"\n+"
-        t.lexer.lineno += t.value.count("\n")
-
-t_ignore = " \t"
-
-def t_error(t):
-    print("unknown character: %s" % t.value)
-
-lex.lex()
