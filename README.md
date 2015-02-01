5a564da6ba867b804ac57cd82fb734e75621c8d7
Noah Kim <noahbkim@gmail.com>
2015-02-01 16:32:40 -0500

    Ooh baby

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
 Rocket
 ======
 
+About
+-----
 A revolutionary new programming language oriented towards learning algorithms.
+
+Syntax
+------
+
+Comments
+	
+	# Comment
+
+Input and Output
+	
+	\<a-zA-Z>  # Outputs variable to stream
+	
+Literals
+	
+	1, 1.00    # Number
+	[1, 2.00]  # List
+
+Variables
+
+	<a-z>  # Number variables 1
+	<A-Z>  # List variables [1, 2, 3]
+
+Identity Operations
+
+	<a-zA-Z>:expression  # Set number or list
+	<a-zA-Z>             # Get number or list 
+	A.b:expression       # Set list element at index b
+	A.b                  # Get list element at index b
+
+Number Operations
+
+	+  # Add
+	-  # Subtract
+	*  # Multiply
+	/  # Divide
+	%  # Modulus
+	^  # Power
+
+List Operations
+
+	$  # Length
+
+Comparisons
+
+	=  # Equal 
+	>  # Greater
+	<  # Less
+
+Boolean
+
+	&  # And
+	|  # Or
+	~  # Not
+
+Grouping
+
+	(<Expression>)  # Higher precedence operation
+
+Control
+
+	?(<expression>){}  # If
+	!(<expression>){}  # While
--- rocket.py
+++ rocket.py
@@ -1,133 +1,43 @@
 # Rocket
 # David Witten, Noah Kim
 
-"""
-
-Rocket Syntax
-
-Comments
-#Comment
-
-Input and Output
-\<a-zA-Z>   outputs variable to stream
-
-Identities
-<a-z>   number variables 1
-<A-Z>   list variables [1, 2, 3]
-
-Identity Operations
-<a-zA-Z>:expression      set number or list
-<a-zA-Z>                 get number or list 
-A.b:expression           set list element at index b
-A.b                      get list element at index b
-
-Number Operations
-+   add
--   subtract
-*   multiply
-/   divide
-%   mod
-^   power
-
-List Operations
-$   length
-
-Comparisons
-=    equal 
->    greater
-<    less
-
-Boolean
-&   and
-|   or
-~   not
-
-Grouping
-(...)   higher precedence operation
-
-Control
-?(<expression>){}     if
-!(<expression>){}     while
-
-"""
-
 # Import
 import fractions
 from ply import lex
 from ply import yacc
 
+# Setup
+namespace = {}
+
 # Lexer
-tokens = [
-    "COMMENT",
-
-    "BACKSLASH",
-    
-    "NUMBER",
-    "NUMBERVARIABLE", "LISTVARIABLE",
-
-    "SQUOTE", "DQUOTE",
-    
-    "PLUS", "MINUS", "TIMES", "DIVIDE", "MOD", "POWER",
-    "DOLLAR",
-
-    "EQUAL", "GREATER", "LESS",
-    "AND", "OR", "NOT",
-
-    "QUESTION", "EXCLAMATION",
-    
-    "LPAREN", "RPAREN",
-    "LBRACKET", "RBRACKET",
-    "LBRACE", "RBRACE",
-    
-    "SEMICOLON", "COLON",
-    "COMMA", "PERIOD"
+tokens = ["COMMENT", "NUMBER", "NVAR", "LVAR"]
+literals = [
+    "\\",
+    ":",
+    "+", "-", "*", "/", "%", "^", "$",
+    "=", ">", "<", "&", "|", "~",
+    "?", "!",
+    ",", ".", ";"
 ]
 
 def t_COMMENT(t):
     r"\#.*"
     pass
 
-t_BACKSLASH = r"\\"
-
 def t_NUMBER(t):
     r"\d+(\.d+)?"
     t.value = fractions.Fraction(t.value)
     return t
 
-t_NUMBERVARIABLE = r"[a-z]"
-t_LISTVARIABLE = r"[A-Z]"
+t_NVAR = r"[a-z]"
+t_LVAR = r"[A-Z]"
 
-t_SQUOTE = r"'"
-t_DQUOTE = r"\""
-
-t_PLUS = r"\+"
-t_MINUS = r"\-"
-t_TIMES = r"\*"
-t_DIVIDE = r"/"
-t_MOD = r"%"
-t_POWER = r"\^"
-t_DOLLAR = r"\$"
-
-t_EQUAL = r"="
-t_GREATER = r">"
-t_LESS = r"<"
-t_AND = r"&"
-t_OR = r"\|"
-t_NOT = r"~"
-
-t_QUESTION = r"\?"
-t_EXCLAMATION = "!"
 t_LPAREN = r"\("
 t_RPAREN = r"\)"
 t_LBRACKET = r"\["
 t_RBRACKET = r"\]"
 t_LBRACE = r"\{"
 t_RBRACE = r"\}"
-
-t_SEMICOLON = r";"
-t_COLON = r":"
-t_COMMA = r","
-t_PERIOD = r"\."
 
 def t_newline(t):
     r'\n+'
@@ -140,11 +50,9 @@ def t_error(t):
     t.lexer.skip(1)
 
 lex.lex()
-s = "a:1;?(a>0){a:a+1;}{\\a;}"
-lex.input(s)
+#s = "a:1;?(a>0){a:a+1;}{\\a;}"
+#lex.input(s)
 
-while True:
-    token = lex.token()
-    if not token: break      # No more input
-    print(token)
+
+
 
