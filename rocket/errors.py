69d889be50b7e598b08ad2945fbcbe52bef3f37e
Noah Kim <noahbkim@gmail.com>
2016-04-15 10:20:25 -0400

    Added a bunch of symbols I'll never implement
    Yolo.

--- rocket/errors.py
+++ rocket/errors.py
@@ -1,7 +1,16 @@
+class ERROR:
+    """Error templates."""
+
+    LOCATION = "%(file)s on line %(line)i"
+    
+    ERROR = "Error in " + LOCATION + ":\n  %(reason)s"
+    SYNTAX = "Syntax error in " + LOCATION + ":\n  Invalid symbol: %(char)s"
+
+
 class RocketError:
     """Base type for Rocket errors."""
 
-    template = "Error in %(file)s on line %(line)i: %(reason)s"
+    template = ERROR.ERROR
     
     def __init__(self, file, line):
         """Initialize a new error."""
@@ -14,8 +23,15 @@ class RocketError:
     def __str__(self):
         return self.template % self
 
+
 class RocketSyntaxError(RocketError):
     """Generic syntax error that is raised by the parser."""
 
-    template = "Syntax Error in %(file)s on line %(line)i: Invalid symbol"
+    template = ERROR.SYNTAX
 
+    def __init__(self, file, line, char):
+        """Initialize the syntax error."""
+
+        super().__init__(file, line)
+        self.char = char
+        
--- rocket/lexer.py
+++ rocket/lexer.py
@@ -4,28 +4,34 @@
 
 # Import
 import fractions
-import rocket.errors
-
-# Tweaks
-def __nice__(fraction):
-    """Get the aesthetically nice value of a fraction."""
-    value = fraction._numerator / fraction._denominator
-    return str(int(value) if int(value) == value else value)
-fractions.Fraction.__repr__ = __nice__
-fractions.Fraction.__str__ = __nice__
+from rocket import errors
 
 # Token List
 tokens = [
     "COMMENT", 
     "NUMBER", "VARIABLE",
+    
     "QUESTION", "EXCLAMATION",
-    "COLON", "SEMI", "COMMA",
-    "ADD", "SUBTRACT", "MULTIPLY", "DIVIDE", "MODULUS", "POWER", "AT", "DOLLAR",
-    "EQUALS", "GREATER", "LESS", "AND", "OR", "NOT",
+    "EQUALS", "COLON", "SEMI", "COMMA",
+
+    "ADD", "SUB", "MUL", "DIV", "MOD", "POW",
+    "ADDEQ", "SUBEQ", "MULEQ", "DIVEQ", "MODEQ", "POWEQ",
+    
+    "AT", "DOLLAR",
+    
+    "EQ", "GT", "LT", "GE", "LE", "NE",
+
+    "ARROW",
+
+    "TILDE",
+    
+    "NOT", "AND", "OR", "XOR",
+    "ANDEQ", "OREQ", "XOREQ",
+    
     "LPAREN", "RPAREN", "LBRACKET", "RBRACKET", "LBRACE", "RBRACE",
 ]
 
-# Token Definitions
+# Core structures
 def t_COMMENT(t):
     r"\#.*"
     pass
@@ -37,29 +43,55 @@ def t_NUMBER(t):
 
 t_VARIABLE = r"[a-zA-Z_][a-zA-Z0-9_]*"
 
+# Basic computation
+t_EQUALS = r"="
+t_ADD = r"\+"
+t_SUB = r"-"
+t_MUL = r"\*"
+t_DIV = r"/"
+t_MOD = r"%"
+t_POW = r"\*\*"
+
+# Mutating computation
+t_ADDEQ = r"\+="
+t_SUBEQ = r"-="
+t_MULEQ = r"\*="
+t_DIVEQ = r"/="
+t_MODEQ = r"%="
+t_POWEQ = r"\*\*="
+
+# Comparison
+t_EQ = "=="
+t_GT = r"<"
+t_LT = r">"
+t_GE = r">="
+t_LE = r"<="
+t_NE = r"!="
+
+# Logic
+t_NOT = r"~"
+t_AND = r"&"
+t_OR = r"\|"
+t_XOR = r"\^"
+
+# Mutating logic
+t_ANDEQ = r"&="
+t_OREQ = r"\|="
+t_XOREQ = r"\^="
+
+# Extended operators
+t_QUESTION = r"\?"
+t_EXCLAMATION = r"!"
+t_ARROW = r"->"
+t_DOLLAR = r"\$"
+t_AT = r"@"
+
+# Syntax
 t_COLON = r":"
 t_SEMI = r";"
 t_COMMA = r","
-t_QUESTION = r"\?"
-t_EXCLAMATION = r"!"
 
-t_ADD= r"\+"
-t_SUBTRACT = r"-"
-t_MULTIPLY = r"\*"
-t_DIVIDE = r"/"
-t_MODULUS = r"%"
-t_POWER = r"\^"
-
-t_AT = r"@"
-t_DOLLAR = r"\$"
-
-t_EQUALS = r"="
-t_GREATER = r"<"
-t_LESS = r">"
-t_AND = r"&"
-t_OR = r"\|"
-t_NOT = r"~"
-
+# Groups
 t_LPAREN = r"\("
 t_RPAREN = r"\)"
 t_LBRACKET = r"\["
@@ -67,12 +99,14 @@ t_RBRACKET = r"\]"
 t_LBRACE = r"\{"
 t_RBRACE = r"\}"
 
+# Whitespace
 def t_newline(t):
-    r'\n+'
+    r"\n+"
     t.lexer.lineno += len(t.value)
 
+# Errors
 def t_error(t):
-    print(" % (t.value[0], t.lexpos, t.lineno))
+    print(errors.RocketSyntaxError(t.lexpos, t.lineno, t.value[0]))
     t.lexer.skip(1)
 
 t_ignore = " \t"
